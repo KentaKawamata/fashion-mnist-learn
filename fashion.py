30e7b6cb728874c06e6d2a62b28ea75ae1d8af06
Ryota9000 <s16c1044sc@s.chibakoudai.jp>
2018-06-27 11:54:49 +0900

    fix miss

--- fashion.py
+++ fashion.py
@@ -28,7 +28,6 @@ def CNN(input_shape):
 
 def plot_history(history):
 
-  score = model.evaluate(x_test, y_test, verbose=0)
   print('Test loss:', score[0])
   print('Test accuracy:', score[1])
 
@@ -91,7 +90,7 @@ def main():
 
   plot_history(history)
   
-  #loss, acc = model.evaliate(x_test, y_test, verbose=0)
+  loss, acc = model.evaliate(x_test, y_test, verbose=0)
 
   print('save the architecture of a CNN model')
   json_string = model.to_json()
